6b321d479bbb08a7ceee5cd240a8b84013473fba
maximv <maximv@cynet.com>
2020-07-08 19:47:08 +0300

    initial commit

---
+++ LempDotnetTool/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace LempDotnetTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(' ', args));
+            // Console.WriteLine(Directory.GetCurrentDirectory());
+            Process.Start(".\\LEMP\\LEMP.exe");
+        }
+    }
+}
