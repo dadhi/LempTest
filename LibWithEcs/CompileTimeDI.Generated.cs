9794a5e2dfc07bcb13f9f519c43e62331da6f64b
maximv <maximv@cynet.com>
2020-07-09 22:20:10 +0300

    making sense of compileTimeAndRuntime, precompute, compileTime and quote.Args

--- LibWithEcs/CompileTimeDI.Generated.cs
+++ LibWithEcs/CompileTimeDI.Generated.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-
 namespace LibWithEcs
 {
     public partial class CompileTimeDI
@@ -13,8 +12,11 @@ namespace LibWithEcs
             Factories = new Dictionary<Type, Expression<Func<object>>>();
         }
     }
-
-    rawPrecompute(LNode.Call(CodeSymbols.Fn, LNode.List(LNode.Id(CodeSymbols.Void), LNode.Id((Symbol) "Z"), LNode.Call(CodeSymbols.AltList), LNode.Call(CodeSymbols.Braces).SetStyle(NodeStyle.StatementBlock))))
 }
-
-	//includeFile("ServiceRegistrations.ecs.include")
+namespace LibWithEcs
+{
+    partial class CompileTimeDI
+    {
+        public static CompileTimeDI Default = new CompileTimeDI();
+    }
+}
