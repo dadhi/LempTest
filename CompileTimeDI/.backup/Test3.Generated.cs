3a9b6a407d64dfe9e7cfbb163e7fcdbdaf00d8af
maximv <maximv@cynet.com>
2021-01-18 14:29:48 +0300

    using the macro with the StringBuilder and runSequence

--- CompileTimeDI/.backup/Test3.Generated.cs
+++ CompileTimeDI/.backup/Test3.Generated.cs
@@ -5,7 +5,7 @@ static class X
     internal static bool TryGetUsedInstance(this IResolverContext r, int serviceTypeHash, Type serviceType, out object instance)
     {
         instance = null;
-        return r.CurrentScope ? .TryGetUsedInstance(r, serviceTypeHash, serviceType, out instance) == true 
-        || r.SingletonScope.TryGetUsedInstance(r, serviceTypeHash, serviceType, out instance) : @__empty__;
+        return r.CurrentScope?.TryGetUsedInstance(r, serviceTypeHash, serviceType, out instance) == true 
+        || r.SingletonScope.TryGetUsedInstance(r, serviceTypeHash, serviceType, out instance);
     }
 }
